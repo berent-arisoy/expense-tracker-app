55a26f770f1f32547fce0556fa1559b5a4d1679c
Berent <berent2002@hotmail.com>
2025-06-12 23:02:02 +0200

    added comments

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
 host: 'localhost',
-user: 'director',
-password: '12345',
+user: 'director',   // Please change it according to your user accounts !!
+password: '12345',  // Same as here !!
 database: 'expense_tracker',
 });
 
